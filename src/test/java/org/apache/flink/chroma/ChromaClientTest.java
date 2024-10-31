5d7a02526360c73250b9300ff7646ee85f749a57
fkzhao <fkzhao@outlook.com>
2024-10-31 11:15:56 +0800

    Initial commit

--- src/test/java/org/apache/flink/chroma/ChromaClientTest.java
+++ src/test/java/org/apache/flink/chroma/ChromaClientTest.java
@@ -1,9 +1,7 @@
 package org.apache.flink.chroma;
-import org.apache.flink.chroma.conf.AuthType;
 import org.junit.jupiter.api.Test;
 
 class ChromaClientTest {
-    private final ChromaClient chromaClient = ChromaClient.builder().url("127.0.0.1:8000").build();
 
     @Test
     void testHealthCheck() {
